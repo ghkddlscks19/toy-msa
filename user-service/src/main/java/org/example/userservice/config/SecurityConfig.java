01c0775132a2bd525ed1574f611e1e83c4c82c7f
ghkddlscks19 <ghkddlscks19@naver.com>
2024-08-25 17:47:12 +0900

    Users Microservice AuthenticationFilter 추가 및 loadUserByUsername() 구현

--- user-service/src/main/java/org/example/userservice/config/SecurityConfig.java
+++ user-service/src/main/java/org/example/userservice/config/SecurityConfig.java
@@ -1,15 +1,30 @@
 package org.example.userservice.config;
 
+import jakarta.servlet.Filter;
+import org.example.userservice.filter.AuthenticationFilter;
+import org.example.userservice.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final UserService userService;
+    private final Environment env;
+
+    public SecurityConfig(UserService userService, Environment env) {
+        this.userService = userService;
+        this.env = env;
+    }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
@@ -18,16 +33,28 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userService).passwordEncoder(passwordEncoder());
+
+        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
         http
                 .csrf((csrf) -> csrf.disable());
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/users/**").permitAll()
-                        .anyRequest().permitAll());
+                        .requestMatchers("/**").access(
+                                new WebExpressionAuthorizationManager("hasIpAddress('192.168.219.102')")));
+        http
+                .addFilter(getAuthenticationFilter(authenticationManager));
         http
                 .headers((header) -> header
                         .frameOptions((frame) -> frame.disable()));
 
         return http.build();
     }
+
+    private AuthenticationFilter getAuthenticationFilter(AuthenticationManager authenticationManager) throws Exception{
+        return new AuthenticationFilter(authenticationManager);
+    }
+
 }
--- user-service/src/main/java/org/example/userservice/service/UserServiceImpl.java
+++ user-service/src/main/java/org/example/userservice/service/UserServiceImpl.java
@@ -7,6 +7,8 @@ import org.example.userservice.repository.UserRepository;
 import org.example.userservice.vo.ResponseOrder;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -25,6 +27,19 @@ public class UserServiceImpl implements UserService{
     public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity findUser = userRepository.findByEmail(username);
+
+        if (findUser == null) {
+            throw new UsernameNotFoundException(username);
+        }
+
+        return new User(findUser.getName(), findUser.getEncryptedPassword(),
+                true, true, true, true,
+                new ArrayList<>());
     }
 
     @Override
